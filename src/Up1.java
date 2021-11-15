bc9b6dfa5187ae3a4c47fc20f3c14a0f5af15f19
Edwimon <edwin.o.h@hotmail.se>
2021-11-15 11:17:05 +0100

    Test

---
+++ src/Up1.java
@@ -0,0 +1,37 @@
+
+import javax.swing.*;
+
+
+public class Up1 {
+    public static void main(String[] args) {
+        String n = JOptionPane.showInputDialog("Enter the thing example (wasdds)");
+        int N = n.length();
+        int pos = 0;
+        int x = 0;
+        int y = 0;
+        for(int i = 0; i <= N; i++){
+
+            if('a' == n.charAt(pos)){
+                x = x-1;
+                pos++;
+            }
+            else if('d' == n.charAt(pos)){
+                x = x+1;
+                pos++;
+            }
+            else if('s' == n.charAt(pos)){
+                y = y-1;
+                pos++;
+            }
+            else if('w' == n.charAt(pos)){
+                y++;
+                pos++;
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "Something went wrong");
+            }
+
+        }
+        JOptionPane.showMessageDialog(null, "(" + x + "," + y + ")");
+    }
+}
